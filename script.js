30016edfbb4eddb8282874651fccbd3d6a2fdea5
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-07-18 22:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    // Create a hash map to store character counts
+    const charCount = {};
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str[i]] === 1) {
+            return str[i];
+        }
+    }
+
+    return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
